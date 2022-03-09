5e83c7d60a437e55da865fd3fdcd31dfc49c4eb7
jihyun <z1hyun@sinsabridge.com>
2022-03-09 21:51:45 +0900

    영속영역 CRUD 구현 완료 / log4j 오류 해결 / INSERT , UPDATE , DELETE , SELECT

--- ex02/src/main/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02/src/main/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -17,23 +17,69 @@ import org.zerock.mapper.BoardMapper;
 public class BoardMapperTests {
 
     @Setter(onMethod_ = @Autowired)
-        private BoardMapper mapper;
+    private BoardMapper mapper;
 
-
-        @Test
-        public void testGetList(){
-            mapper.getList().forEach(board -> log.info(board));
-         }
-
-//        @Test
-//        public void testInsert() {
-//            BoardVO board = new BoardVO();
-//            board.setTitle("새로 작성하는 글~~");
-//            board.setContent("새로 작성하는 글의 내용!!!");
-//            board.setWriter("newbie~~~!");
+////
+////        @Test
+////        public void testGetList(){
+////            mapper.getList().forEach(board -> log.info(board));
+////         }
 //
-//            mapper.insert(board);
-//            log.info(board);
-//        }
+//
+//    @Test
+//    public void testInsert() {
+//        BoardVO board = new BoardVO();
+//        board.setTitle("새로작성하는 글입니다~~!! ");
+//        board.setContent("새로 작성하는 내용");
+//        board.setWriter("newbie!!!");
+//
+//        mapper.insert(board);
+//
+//        log.info(board);
+//    }
+//
+//    @Test
+//    public void testInsertSelectKey() {
+//        BoardVO board = new BoardVO();
+//        board.setTitle("새로작성하는 select key~~~");
+//        board.setContent("select key의 내용");
+//        board.setWriter("뉴비!!!");
+//
+//        mapper.insertSelectKey(board);
+//
+//        log.info(board);
+//    }
+//
+//
+//    @Test
+//    public void testRead() {
+//
+//        BoardVO board = mapper.read(3L);
+//        //long 타입이라 숫자뒤에 L을 넣나보다.
+//
+//        log.info(board);
+//    }
 
+//
+//    @Test
+//    public void testDelete(){
+//
+//        log.info("DELETE COUNT::::"+mapper.delete(2L));
+//        //bno에 숫자는 뭘까.. 이건 내일 알아보자!!!꼭 !!!
+//}
+
+    @Test
+    public void testUpdate(){
+
+        BoardVO board = new BoardVO();
+
+        board.setBno(11L);
+        board.setTitle("수정되었습니다!!");
+        board.setContent("수정된 내용입니다!~~");
+        board.setWriter("새로운 user ㅎㅎ");
+
+        int count = mapper.update(board);
+        log.info("UPDATE COUNT:::"+count);
+    }
 }
+
